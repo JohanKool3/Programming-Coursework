9772874290a2aee439a73f89c92bdc1a6e88b69e
Ecstatik <johankool2003@hotmail.co.uk>
2023-02-16 19:24:39 +0000

    Added final backend object

--- backend.py
+++ backend.py
@@ -3,6 +3,47 @@
 """ 
     Beginning of the class library for the backend.
 """
+class SmartHome(object):
+
+    """
+    A class that represents a smart home.
+    """
+    def __init__(self):
+        self.devices = []
+
+    def __str__(self):
+        output = "Smart Home: \n"
+
+        for index in range(len(self.devices)):
+            output += f" {index + 1}. {self.devices[index]} \n"
+
+        return output
+
+    def addDevice(self, device):
+        self.devices.append(device)
+
+    def getDevices(self):
+        return self.devices
+
+    def getDeviceat(self, index):
+        return self.devices[index]
+
+    def toggleSwitch(self, index):
+        self.devices[index].toggleSwitch()
+
+    def turnoffAllDevices(self):
+        for device in self.devices:
+
+            if device.getSwitchState():
+                device.toggleSwitch()
+
+    def turnonAllDevices(self):
+        for device in self.devices:
+
+            if not device.getSwitchState():
+                device.toggleSwitch()
+
+
 class SmartPlug(object):
     """ 
     A class that represents a simple smart plug.
@@ -100,4 +141,22 @@ def testSmartWashingMachine():
         print(machine)
 
 
-testSmartWashingMachine()
+def testSmartHome():
+
+    home = SmartHome()
+    plug1 = SmartPlug()
+    plug2 = SmartPlug()
+    smWshMch = SmartWashingMachine()
+
+    home.addDevice(plug1)
+    home.addDevice(plug2)
+    home.addDevice(smWshMch)
+
+    plug2.toggleSwitch()
+    plug2.setConsumptionRate(45)
+    smWshMch.setWashMode("Quick wash")
+    print(home)
+    home.turnonAllDevices()
+    print(home)
+
+# testSmartHome()
